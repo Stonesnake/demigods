073ab8ad995091f592581422e01e64a5a2c186a1
Ian Blackstone <ianblackstone@gmail.com>
2025-03-26 23:10:51 -0400

    Split CSS by system

--- scripts/register-journal-style.mjs
+++ scripts/register-journal-style.mjs
@@ -2,10 +2,24 @@ import CONFIG from "./module.mjs";
 
 Hooks.on("renderJournalSheet", (app, html, data) => {
   const journal = app.document;
-  if ( journal.getFlag(CONFIG.moduleId, CONFIG.journalFlag) ) html[0].classList.add(CONFIG.cssClass);
+  if ( journal.getFlag(CONFIG.moduleId, CONFIG.journalFlag)) {
+    if (game.system.id === "pf2e") {
+      html[0].classList.add(CONFIG.cssClassPF2e);
+      }
+    else {
+      html[0].classList.add(CONFIG.cssClass5e);
+    }
+  }
 });
 
 Hooks.on("renderJournalPageSheet", (app, html, data) => {
   const journal = app.document.parent;
-  if ( journal.getFlag(CONFIG.moduleId, CONFIG.journalFlag) ) html[0].classList.add(CONFIG.cssClass);
+  if ( journal.getFlag(CONFIG.moduleId, CONFIG.journalFlag) ) {
+    if (game.system.id === "pf2e") {
+      html[0].classList.add(CONFIG.cssClassPF2e);
+      }
+    else {
+      html[0].classList.add(CONFIG.cssClass5e);
+    }
+  }
 });
